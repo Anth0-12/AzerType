376c0086bc88b3fcbaa7701860a3e202c60ee393
Anth0-12 <antho62680@live.fr>
2023-10-07 00:45:05 +0200

    azertype

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-lancerJeu();
+// lancerJeu();
+
+let inputEcriture = document.getElementById("inputEcriture");
+console.log(inputEcriture);
+
+let boutonValider = document.getElementById("btnValiderMot");
+console.log(boutonValider);
+
+let zoneProposition = document.querySelector(".zoneProposition");
+console.log(zoneProposition);
+
+let zoneScore = document.querySelector(".zoneScore span");
+console.log(zoneScore);
+
+let inputRadio = document.querySelectorAll(".optionSource input");
+console.log(inputRadio);
