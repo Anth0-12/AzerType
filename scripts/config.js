b1d3bd9cdb0d46558ec81936005fa30e83635090
Anth0-12 <antho62680@live.fr>
2023-10-06 18:51:45 +0200

    entrainement js

---
+++ scripts/config.js
@@ -0,0 +1,8 @@
+// liste de tableaux
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+const listePhrases = [
+  "Pas de panique !",
+  "La vie, l'univers et le reste",
+  "Merci pour le poisson",
+  "La vie en rose",
+];
---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function afficherResultat(score, nbMotsProposes) {
+  console.log("Votre score est de " + score + " sur " + nbMotsProposes);
+}
+
+function choisirPhrasesOuMots() {
+  //on demande de choir entre "mots" ou "phrases"
+  let choix = prompt("Veuillez choisir la liste: mots ou phrases");
+
+  //tant qu'il n'a pas choisi "mots" ou "phrases" on redemande
+  while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Veuillez choisir la liste: mots ou phrases");
+  }
+  return choix;
+}
+
+function lancerBoucleDeJeu(listePropositions) {
+  let score = 0;
+
+  //tant que la longueur de la liste n'est pas atteinte, on demande le mot suivant
+  for (let i = 0; i < listePropositions.length; i++) {
+    motUtilisateur = prompt("Ecrivez : " + listePropositions[i]);
+
+    //si le mot est exact, alors le score augmente de 1
+    if (motUtilisateur === listePropositions[i]) {
+      score++;
+    }
+  }
+  return score;
+}
+
+function lancerJeu() {
+  let choix = choisirPhrasesOuMots();
+
+  let score = 0;
+
+  let nbMotsProposes = 0;
+
+  if (choix === "mots") {
+    score = lancerBoucleDeJeu(listeMots);
+    nbMotsProposes = listeMots.length;
+  } else {
+    score = lancerBoucleDeJeu(listePhrases);
+    nbMotsProposes = listePhrases.length;
+  }
+
+  afficherResultat(score, nbMotsProposes);
+}
